d41bb7dbff3748af72d687536c61422d50c13bee
ARO <roqeeboloruntele@gmail.com>
2023-10-15 01:50:23 +0100

    Array comparison exercise attempted

---
+++ Fundamentals-of-Programming/bash-and-shell/array-comparison.sh
@@ -0,0 +1,9 @@
+	#!/bin/bash
+	# enter your array comparison code here
+    
+    a=(3 5 8 10 6)
+    b=(6 5 4 12)
+    c=(14 7 5 7) 
+    
+    # IF the number in the a b and c array are not the same, continue
+    echo ${#a[@]}
